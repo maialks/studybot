5c56be1e59e841a1e762d03112fec2bf2cb02ba0
maialks <lucasmss01ita@gmail.com>
2025-10-17 09:38:49 -0300

    minor fixes

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -3,3 +3,18 @@ import logger from './utils/general/logger';
 
 const app = new App();
 app.start().catch((error) => logger.error(error));
+
+// Captura erros globais não tratados
+process.on('unhandledRejection', (reason, _promise) => {
+  logger.error('❌ Unhandled Promise Rejection:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  logger.error('❌ Uncaught Exception:', error);
+  process.exit(1);
+});
+
+process.on('SIGINT', async () => {
+  logger.info('❌ Encerrando aplicação...');
+  process.exit(0);
+});
